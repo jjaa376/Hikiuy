9dae50d587ea9470c083ccbf56dc8bde850a9a3f
jjaa376 <ops5mellion@gmail.com>
2026-01-04 22:22:38 +0600

    Use environment variable for port configuration

--- api.py
+++ api.py
@@ -271,4 +271,5 @@ def uid_route():
     except Exception as e: return custom_jsonify({"error": str(e)}, 500)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
